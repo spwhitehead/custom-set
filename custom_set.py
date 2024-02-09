1f23560c9b32ed3c698d5da7982dfc25ec601c17
Spencer Whitehead <spencerpwhitehead@gmail.com>
2024-02-09 08:55:09 -0700

    Define each method and state what they do Add in the arguments for each method and state what each method is meant to do.

--- custom_set.py
+++ custom_set.py
@@ -2,16 +2,20 @@ class CustomSet:
     def __init__(self):
         pass
 
-    def add(self):
+    def add(self, item: str):
+        """Add an item to the list"""
         pass
 
-    def remove(self):
+    def remove(self, item: str):
+        """Remove an item from the list"""
         pass
 
     def as_list(self):
+        """Returns all the items in the list"""
         pass
 
     def clear(self):
+        """Removes all the items from the list"""
         pass
 
 def main():
