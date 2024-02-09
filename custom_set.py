3522d43c1d265954dd889f9ecdcfa1fb4cc44918
Spencer Whitehead <spencerpwhitehead@gmail.com>
2024-02-09 08:46:28 -0700

    Set up methods Set up the methods as defined in the project requirements.

---
+++ custom_set.py
@@ -0,0 +1,23 @@
+class CustomSet:
+    def __init__(self):
+        pass
+
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def as_list(self):
+        pass
+
+    def clear(self):
+        pass
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
